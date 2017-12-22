6cda126ceaea916ca9aeecb34977674476678105
swizrc <swizrc@mst.edu>
2017-12-21 18:06:46 -0600

    Replaced standard CheeseId label when adding menu items

--- src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
+++ src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace CheeseMVC.ViewModels
 {
     public class AddMenuItemViewModel
     {
+        [Display(Name = "Cheese")]
         public int cheeseID { get; set; }
         public int menuID { get; set; }
 
